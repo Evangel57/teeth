45ec2c6f6cde5b0d4d3f14c5205b4580e437e811
Evangel57 <124142516+Evangel57@users.noreply.github.com>
2024-01-14 23:02:04 +0700

    add about

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-// Get all the anchor elements
-const links = document.querySelectorAll('a');
+// // Get all the anchor elements
+// const links = document.querySelectorAll('a');
 
-// Add event listeners to each anchor element
-links.forEach(link => {
-  link.addEventListener('click', function() {
-    // Remove active class from all links
-    links.forEach(otherLink => {
-      otherLink.classList.remove('active');
-    });
+// // Add event listeners to each anchor element
+// links.forEach(link => {
+//   link.addEventListener('click', function() {
+//     // Remove active class from all links
+//     links.forEach(otherLink => {
+//       otherLink.classList.remove('active');
+//     });
     
-    // Add active class to the clicked link
-    this.classList.add('active');
+//     // Add active class to the clicked link
+//     this.classList.add('active');
+//   });
+// });
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").classList.add("show");
+    document.getElementById("scrollToTopBtn").classList.remove("hide");
+  } else {
+    document.getElementById("scrollToTopBtn").classList.add("hide");
+    document.getElementById("scrollToTopBtn").classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
   });
-});
+}
